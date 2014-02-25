ee5098b778f8c8f58a1844b76f3292c48ebc6a5e
Karan Batra-Daitch <karanganesha04@gmail.com>
2014-02-25 02:12:41 -0500

    Refactor code to use angular scope best practice of dot in ngModels

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,40 +3,46 @@
 angular.module('myApp')
   .controller('MainCtrl', (['$scope', '$http', 'venueService', function ($scope, $http, venueService) {
     
-    $scope.boroughs = ['bronx', 'brooklyn', 'queens', 'manhattan'];
-    $scope.venuesMeta = {};
+    $scope.input = {
+      venuesResource: {},
+      boroughs: ['bronx', 'brooklyn', 'queens', 'manhattan']
+    };
+
+    $scope.output = {
+      venuesMeta: {}
+    };
+
+    // Local instance for code-readability
+    var boroughs = $scope.input.boroughs;
 
     // Fetch the venues using the service that returns an ngResource
-    $scope.venuesResource = venueService().get(function() {
-      // On resolved promise, setup initial state
-      $scope.borough = 'bronx';
-      $scope.venuesMeta[$scope.borough] = $scope.venuesResource[$scope.borough].response.groups[0].items;
+    $scope.input.venuesResource = venueService().get(function() {
+      // On resolved promise, setup various $watch
+      // These $watch calls are performed on selectors in the DOM
 
-      // Register watch on select filter
-      $scope.$watch ('borough', function(newBorough) {
-        if (newBorough) {
-          // Clear-out any unselected boroughs
-          for (var i = 0; i < $scope.boroughs.length; i++) {
-            if ($scope.boroughs[i] != newBorough) {
-              $scope.venuesMeta[$scope.boroughs[i]] = [];
-            }
+      $scope.$watch ('boroughSelector', function(newBorough) {
+        if (newBorough && newBorough.length) {
+          $scope.output.venuesMeta = {};
+
+          for (var i = 0; i < newBorough.length; i++) {
+            $scope.output.venuesMeta[newBorough[i]] = $scope.input.venuesResource[newBorough[i]].response.groups[0].items;
           }
-          // Select newly selected borough
-          $scope.venuesMeta[newBorough] = $scope.venuesResource[newBorough].response.groups[0].items;
         } else {
           // Select all boroughs
-          for (var i = 0; i < $scope.boroughs.length; i++) {
-            $scope.venuesMeta[$scope.boroughs[i]] = $scope.venuesResource[$scope.boroughs[i]].response.groups[0].items;
+          for (var i = 0; i < boroughs.length; i++) {
+            $scope.output.venuesMeta[boroughs[i]] = $scope.input.venuesResource[boroughs[i]].response.groups[0].items;
           }
         }
       });
     });
 
+    // Computed Properties
     $scope.venuesCount = function() {
       var count = 0;
-      for (var i = 0; i < $scope.boroughs.length; i++) {
-        if ($scope.venuesMeta[$scope.boroughs[i]]) {
-          count += $scope.venuesMeta[$scope.boroughs[i]].length;
+      var boroughs = $scope.input.boroughs;
+      for (var i = 0; i < boroughs.length; i++) {
+        if ($scope.output.venuesMeta[boroughs[i]]) {
+          count += $scope.output.venuesMeta[boroughs[i]].length;
         }
       }
       return count;
