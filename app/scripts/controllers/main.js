2e0e614e46689e01ee97d54c156c5d2a77d79875
Karan Batra-Daitch <karanganesha04@gmail.com>
2014-02-24 22:56:14 -0500

    Add logic to display all boroughs as well as single

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,20 +2,33 @@
 
 angular.module('myApp')
   .controller('MainCtrl', (['$scope', '$http', 'venueService', function ($scope, $http, venueService) {
-
     
-
     $scope.boroughs = ['bronx', 'brooklyn', 'queens', 'manhattan'];
+    $scope.venuesMeta = {};
 
     // Fetch the venues using the service that returns an ngResource
     $scope.venuesResource = venueService().get(function() {
       // On resolved promise, setup initial state
       $scope.borough = 'bronx';
-      $scope.venuesMeta = $scope.venuesResource[$scope.borough].response.groups[0].items;
+      $scope.venuesMeta[$scope.borough] = $scope.venuesResource[$scope.borough].response.groups[0].items;
 
       // Register watch on select filter
       $scope.$watch ('borough', function(newBorough) {
-        $scope.venuesMeta = $scope.venuesResource[$scope.borough].response.groups[0].items;
+        if (newBorough) {
+          // Clear-out any unselected boroughs
+          for (var i = 0; i < $scope.boroughs.length; i++) {
+            if ($scope.boroughs[i] != newBorough) {
+              $scope.venuesMeta[$scope.boroughs[i]] = {};
+            }
+          }
+          // Select newly selected borough
+          $scope.venuesMeta[newBorough] = $scope.venuesResource[newBorough].response.groups[0].items;
+        } else {
+          // Select all boroughs
+          for (var i = 0; i < $scope.boroughs.length; i++) {
+            $scope.venuesMeta[$scope.boroughs[i]] = $scope.venuesResource[$scope.boroughs[i]].response.groups[0].items;
+          }
+        }
       });
     });
   }
