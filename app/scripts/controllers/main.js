fb515b8eff964fe06c47738d279a7dac28c248f1
Karan Batra-Daitch <karanganesha04@gmail.com>
2014-02-25 12:25:00 -0500

    Refactor server response to plain JSON array Refactor angular app to simply use cascaded filters. Doctor manhattan and queens data to return correct borough name in city. Bump angular-chosen version to 1.0.5.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -19,66 +19,10 @@ angular.module('myApp')
     var boroughs = $scope.input.boroughs;
 
     // Fetch the venues using the service that returns an ngResource
-    $scope.input.venuesResource = venueService().get(function() {
-      // On resolved promise, setup various $watch
-      // These $watch calls are performed on selectors in the DOM
-
-      $scope.$watch ('input.boroughSelector', function(newBoroughs) {
-        if ($scope.input.categorySelector) {
-          resetVenuesMetaForCategory($scope, newBoroughs, $scope.input.categorySelector);
-        } else {
-          resetVenuesMeta($scope, newBoroughs);
-        }
-      });
-
-      $scope.$watch('input.categorySelector', function(newCategories) {
-        // First we restore state for selected boroughs, then filter by category
-        if (newCategories && newCategories.length) {
-          resetVenuesMetaForCategory($scope, $scope.input.boroughSelector, newCategories)
-        } else {
-          resetVenuesMeta($scope, $scope.input.boroughSelector);
-        }
-      });
+    $scope.input.venuesResource = venueService().query(function() {
+      // On resolved promise, setup initial state
+      $scope.output.venuesMeta = $scope.input.venuesResource;      
     });
-
-    // Private functions
-    function resetVenuesMeta($scope, boroughs) {
-      var _boroughs;
-
-      if (boroughs && boroughs.length) {
-        _boroughs = boroughs;
-      } else {
-        _boroughs = $scope.input.boroughs;
-      }
-
-      $scope.output.venuesMeta = {};
-
-      for (var i = 0; i < _boroughs.length; i++) {
-        $scope.output.venuesMeta[_boroughs[i]] = angular.copy($scope.input.venuesResource[_boroughs[i]].response.groups[0].items);
-      }
-    }
-
-    function resetVenuesMetaForCategory($scope, boroughs, newCategories) {
-      var _boroughs;
-      if (boroughs) {
-        _boroughs = boroughs;
-      } else {
-        _boroughs = $scope.input.boroughs;
-      }
-
-      $scope.output.venuesMeta = {};
-
-      for (var i = 0; i < _boroughs.length; i++) {
-        var newItems = angular.copy($scope.input.venuesResource[_boroughs[i]].response.groups[0].items);
-
-        for(var j = newItems.length - 1; j >= 0; j--) {
-          if(newCategories.indexOf(newItems[j].venue.categories[0].shortName.toLowerCase()) === -1) {
-            newItems.splice(j, 1);
-          }
-        }
-        $scope.output.venuesMeta[_boroughs[i]] = newItems;
-      }
-    }
 
     // Computed Properties
     $scope.venuesCount = function() {
--- app/scripts/filters.js
+++ app/scripts/filters.js
@@ -2,7 +2,45 @@
 
 angular.module('myApp')
 .filter('upcase', function() {
-  return function(string) {
-      return string.charAt(0).toUpperCase() + string.slice(1);
-    };
+  return upcase;
+})
+.filter('byBoroughAndCategory', function() {
+  return function(venuesMeta, input) {
+    var borough = input.boroughSelector;
+    var category = input.categorySelector;
+
+    if (borough) {
+      venuesMeta = filterByBorough(venuesMeta, borough);
+    }
+
+    if (venuesMeta.length && category) {
+      venuesMeta = filterByCategory(venuesMeta, category);
+    }
+
+    return venuesMeta;
+ }     
 });
+
+function filterByBorough(venuesMeta, borough) {
+  if (!borough) {
+    return venuesMeta;
+  }
+
+  return venuesMeta.filter(function(meta, index, array) {
+    return meta.venue.location.city === upcase(borough);
+  });
+}
+
+function filterByCategory(venuesMeta, category) {
+  if (!category) {
+    return venuesMeta;
+  }
+
+  return venuesMeta.filter(function(meta, index, array) {
+    return meta.venue.categories[0].shortName.toLowerCase() === category;
+  });
+}
+
+function upcase(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
